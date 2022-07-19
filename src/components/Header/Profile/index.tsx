b70e88e892ff786d062a960355c9dee3717793b4
Vítor Marco <vitoradrianomarco@gmail.com>
2022-07-19 20:21:49 -0300

    header responsive

--- src/components/Header/Profile/index.tsx
+++ src/components/Header/Profile/index.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export const Profile = () => {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export const Profile = ({ showProfileData = true }: ProfileProps) => {
   return (
     <Flex align="center">
-      <Box mr={4} textAlign="right">
-        <Text>Vítor Marco</Text>
-        <Text color="gray.300" fontSize="small">
-          vitoradrianomarco@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr={4} textAlign="right">
+          <Text>Vítor Marco</Text>
+          <Text color="gray.300" fontSize="small">
+            vitoradrianomarco@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Vítor Marco"
