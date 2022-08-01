dc7a7532ec027adc30ceb9c39963d76e776ef0f5
Vítor Marco <vitoradrianomarco@gmail.com>
2022-08-01 09:01:01 -0300

    Add users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Model, Factory } from 'miragejs';
+import { faker } from '@faker-js/faker';
 
 interface User {
   name: string;
@@ -12,14 +13,32 @@ export const makeServer = () => {
       user: Model.extend<Partial<User>>({}),
     },
 
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList('user', 200);
+    },
+
     routes() {
       this.namespace = 'api';
       this.timing = 750;
 
       this.get('/users');
       this.post('/users');
-
       this.namespace = '';
+
       this.passthrough();
     },
   });
